5daec1aa1b95162f307d500a93080f9d7b419b6d
Manuel <5673677+mtrezza@users.noreply.github.com>
2021-08-25 02:58:30 +0200

    build

--- dist/index.js
+++ dist/index.js
@@ -6572,7 +6572,7 @@ function composeMessage({requireCheckboxes, requireTemplate, suggestPr} = {}) {
   }
 
   // Add beta note
-  message += `\n\n---\n*I'm still in beta, so forgive me if I'm still making mistakes.*`;
+  message += `\n\n---\n*I'm in beta, so forgive me if I'm still making mistakes.*`;
 
   // Fill placeholders
   message = fillPlaceholders(message, payload);
